9380f71bc9ebab9f66a6418c0c35f9a8ebf32873
Miss Sweety <sweetymiss018@gmail.com>
2024-11-29 10:07:20 +0530

    This is a LMS Project. Serving a purpose of E-learning platform.

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Login from './Pages/Login';
+
+function App() {
+  return (
+    <main>
+      <Login/>
+    </main>
+  )
+}
+
+export default App
